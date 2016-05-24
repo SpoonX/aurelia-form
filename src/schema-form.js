69abddbd9aab78c9dfce1178ec14a024524e0bb4
bas080 <bas080@hotmail.com>
2016-05-24 12:28:40 +0200

    fix(form): typo fix

--- src/schema-form.js
+++ src/schema-form.js
@@ -1,36 +1,8 @@
 import {bindable, bindingMode} from 'aurelia-framework';
 
-export class schemaFormCustomElement {
-
+export class SchemaFormCustomElement {
   @bindable schema
 
   @bindable({defaultBindingMode: bindingMode.twoWay})
   model
-
-  // constructor(formElement) {
-  //   this.element = formElement;
-  // }
-
-  // attached () {
-  //   if (this.model.hasValidation && this.model.hasValidation()) {
-  //     this.validation = this.model.getValidation();
-  //   }
-  // }
-
-  // submit () {
-  //   return this.onSubmit();
-  // }
-
-  // onSubmit () {
-  //   if (this.model.hasValidation && !this.model.hasValidation()) {
-  //     return this.element.dispatchEvent(new CustomEvent('complete', this.model.asObject()));
-  //   }
-
-  //   this.validation.validate()
-  //     .then(() => {
-  //       this.element.dispatchEvent(new CustomEvent('complete', this.model.asObject()));
-  //     })
-  //     .catch(error => {});
-  // }
-
 }
