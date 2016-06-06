a6cc13bccb517c7a862435eeea6a98a9e41d44f4
bas080 <bas080@hotmail.com>
2016-06-06 19:25:52 +0200

    refactor(collection): less code and same results

--- src/frameworks/collection.js
+++ src/frameworks/collection.js
@@ -1,18 +1,8 @@
-import {computedFrom} from 'aurelia-framework';
-
 export class Collection {
 
-  constructor() {}
-
-  /* damn biolerplate */
   activate(model) {
     this.attribute = model.attribute;
     this.models = model.value;
-  }
-
-  @computedFrom('attribute')
-  get schema() {
-    return this.attribute.schema;
   }
 
 }
