71b5b869566ad1561d328d3195d43c7f9de53639
bas080 <bas080@hotmail.com>
2016-06-08 19:05:06 +0200

    feat(Form): the form class which makes writing form a bit easier

---
+++ src/form.js
@@ -0,0 +1,47 @@
+import {Validator, ValidationEngine} from 'aurelia-validatejs';
+
+/***
+ * Some functionalities are desired in a view model of a form. Responsibilites
+ * include validation and submit.
+ *
+ * This class is optional and serves as a convenience
+ */
+export class Form {
+
+  messages = {}
+
+  _model
+
+  constructor() {}
+
+  set model(newModel) {
+    this._model = newModel;
+    this.validator = new Validator(newModel);
+    this.reporter = ValidationEngine.getValidationReporter(newModel);
+    this.observer = this.reporter.subscribe(validationErrors => {
+      this.messages = validationErrors.reduce( (acc, error) => {
+        acc[error.propertyName] = error.message;
+        return acc;
+      }, {});
+    });
+  }
+
+  get model() {
+    return this._model;
+  }
+
+  validate() {
+    this.validator.validate();
+  }
+
+  detached() {
+    this.observer.dispose();
+  }
+
+  submit() {
+  }
+
+  change() {
+    this.validate();
+  }
+}
