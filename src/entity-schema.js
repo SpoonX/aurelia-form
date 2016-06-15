e68fdf836d4dc07827c548fc485ceb76e70f8867
bas080 <bas080@hotmail.com>
2016-06-15 15:55:20 +0200

    fix(entity): builds a form friendly schema using the entity's metadata

--- src/entity-schema.js
+++ src/entity-schema.js
@@ -1,19 +1,31 @@
 /**
- * Takes an entity and uses it's metadata to generate a form-schema.
+ * Takes an entity and uses it's metadata to generate a form-schema. Entities
+ * behave like normal objects in the way one can get and set the values on
+ * it's properties.
  *
  * @param {object} entity
- * @returns {object[]} the schema with the attributes.
+ *
+ * @returns {object[]} a schema consisting out of element objects
  */
 export function entitySchema(entity) {
-  const keys  = Object.keys(entity);
-  let typeMap = entity.getMeta().metadata.types;
-  let schema  = [];
+  let metadata     = entity.getMeta();
+  let types        = metadata.fetch('types') || {};
+  let associations = metadata.fetch('associations');
+  let schema       = [];
 
-  for (let key of keys) {
-    schema.push({
-      key : key,
-      type: typeMap[key]
-    });
+  for (let key of Object.keys(entity)) {
+    if (key === '__validationReporter__') {continue;} /* should be fixed in orm */
+    let element = {
+      key: key,
+      type: types[key]
+    };
+
+    if (associations[key] && associations[key].type === 'collection') {
+      element.type = 'collection';
+      element.schema = entitySchema(entityManager.getEntity(key));
+    }
+
+    schema.push(element);
   }
 
   return schema;
