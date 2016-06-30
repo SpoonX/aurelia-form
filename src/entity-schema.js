9ba164ab4d02fe53be64911ec0cbf8ff8d9063e3
bas080 <bas080@hotmail.com>
2016-06-30 12:18:47 +0200

    feat(components): association type and component

--- src/entity-schema.js
+++ src/entity-schema.js
@@ -1,3 +1,5 @@
+import extend from 'extend';
+
 /**
  * Takes an entity and uses it's metadata to generate a form-schema. Entities
  * behave like normal objects in the way one can get and set the values on
@@ -11,14 +13,18 @@ export function entitySchema(entity) {
   let metadata     = entity.getMeta();
   let types        = metadata.fetch('types') || {};
   let associations = metadata.fetch('associations');
+  let data         = metadata.fetch('data') || {};
   let schema       = [];
 
   for (let key of Object.keys(entity)) {
     if (key === '__validationReporter__') {continue;} /* should be fixed in orm */
+
     let element = {
       key: key,
       type: types[key]
     };
+
+    element = extend(true, element, data[key].form || {});
 
     if (associations[key] && associations[key].type === 'collection') {
       element.type = 'collection';
