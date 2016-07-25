e5d16984138fba59fcfe682c209405642720238f
bas080 <bas080@hotmail.com>
2016-07-25 12:23:25 +0200

    style(component): remove const and propertObserver on newline

--- src/component/framework/conditional.js
+++ src/component/framework/conditional.js
@@ -9,14 +9,15 @@ export class Conditional {
 
   activate(field) {
     this.model            = field.value;
-    const calculateSchema = () => {
+    let calculateSchema = () => {
       this.schema = field.element.schema(this.model);
     };
 
     calculateSchema();
 
     if (typeof this.model === 'object' && field.element.observe) {
-      this.observer = this.bindingEngine.propertyObserver(this.model, field.element.observe)
+      this.observer = this.bindingEngine
+        .propertyObserver(this.model, field.element.observe)
         .subscribe(calculateSchema);
     }
   }
