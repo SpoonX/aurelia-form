7e2bbce84814b2eb24d5be37f20396f3fbe53e32
bas080 <bas080@hotmail.com>
2016-08-03 12:10:12 +0200

    fix(options): use template and value converter for options

--- src/component/framework/options.js
+++ src/component/framework/options.js
@@ -1,13 +1,6 @@
-import {normalizeOptions} from '../../utils';
 import {logger} from '../../aurelia-form';
-import {BindingEngine, inject} from 'aurelia-framework';
 
-@inject(BindingEngine)
 export class Options {
-
-  constructor(bindingEngine) {
-    this.bindingEngine = bindingEngine;
-  }
 
   activate(model) {
     if (!Array.isArray(model.element.options)) {
@@ -16,17 +9,6 @@ export class Options {
 
     this.value   = model.value;
     this.element = model.element;
-    this.options = normalizeOptions(model.element.options);
-
-    /* when options change they have to be renormalized */
-    this.optionsObserver = this.bindingEngine.collectionObserver(model.element.options)
-      .subscribe(() => {
-        this.options = normalizeOptions(model.element.options);
-      });
-  }
-
-  unbind() {
-    this.optionsObserver.dispose();
   }
 
 }
