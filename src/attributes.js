5ed10f096faf855bd45cd9a5ce91341d6ed6e979
bas080 <bas080@hotmail.com>
2016-09-14 10:56:18 +0200

    feat(attributes): normalize attributes to an object
    One can now pass a string, array of strings or an object with string or boolean
    values to the attributes form-element property.

--- src/attributes.js
+++ src/attributes.js
@@ -8,13 +8,38 @@ import {inject} from 'aurelia-dependency-injection';
 export class AttributesCustomAttribute {
 
   constructor(element) {
+    console.log('attributes');
     this.element = element;
   }
 
   valueChanged() {
-    Object.keys(this.value).forEach(attribute => {
+    Object.keys(normalizeAtttibutes(this.value)).forEach(attribute => {
       this.element.setAttribute(attribute, this.value[attribute]);
     });
   }
 
+}
+
+/**
+ * @param {object|string|string[]} value
+ * @returns {object} where all the values are strings or boolean
+ */
+function normalizeAtttibutes(value, result = {}) {
+  console.log(value, result);
+
+  if (typeof this.value === 'string') {
+    result[this.value] = true;
+
+    return result;
+  }
+
+  if (Array.isArray(this.value)) {
+    this.value.forEach(v => {
+      result = normalizeAtttibutes(v, result);
+    });
+
+    return result;
+  }
+
+  return result;
 }
