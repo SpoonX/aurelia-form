ac443fe861e7f3a8f9764608508b859a51fc2b3c
doktordirk <doktordirk@outlook.de>
2016-10-05 16:12:51 +0200

    chore(project) new build

--- dist/amd/component/form-fields.js
+++ dist/amd/component/form-fields.js
@@ -9,7 +9,7 @@ define(['exports', 'aurelia-framework', 'aurelia-view-manager', '../logger'], fu
   var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
     return typeof obj;
   } : function (obj) {
-    return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj;
+    return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
   };
 
   function _initDefineProp(target, property, descriptor, context) {
