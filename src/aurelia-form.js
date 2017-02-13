6f4d8036e073db89aa0e875294d832e64e013759
RWOverdijk <r.w.overdijk@gmail.com>
2017-02-13 15:45:22 +0100

    refactor(project): refactor index for easier component config

--- src/aurelia-form.js
+++ src/aurelia-form.js
@@ -3,28 +3,42 @@ import {getLogger} from "aurelia-logging";
 
 export const logger = getLogger('aurelia-form');
 
-export function configure(aurelia) {
+export function configure(aurelia, config) {
   aurelia.aurelia.use.plugin('aurelia-view-manager');
 
   aurelia.container.get(ViewManagerConfig).configureNamespace('spoonx/form', {
     location: './view/{{framework}}/{{view}}.html'
   });
 
+  let defaultComponents = [
+    'aurelia-form',
+    'form-element',
+    'form-label',
+    'form-button',
+    'form-help',
+    'form-error',
+    'form-group'
+  ];
+
+  let defaultElements = [
+    'input'
+  ];
+
   aurelia.globalResources(
     './attribute/attributes',
     './attribute/prefixed',
-    './component/aurelia-form',
-    './component/form-label',
-    './component/form-input',
-    './component/form-button',
-    './component/form-help',
-    './component/form-error',
-    './component/form-group',
+    ...defaultComponents.map(component => `./component/${component}`),
+    ...defaultElements.map(component => `./component/form-${component}`)
   );
+
+  defaultElements.forEach(element => {
+    config.elements[element] = `form-${element}`
+  });
 }
 
 export const config = {
   'aurelia-form': {
-    // Nothing yet.
+    defaultElement: 'input',
+    components    : {}
   }
 };
