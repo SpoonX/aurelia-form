0171495384e5b104e56fb025bf9322e2fe0385f4
RWOverdijk <r.w.overdijk@gmail.com>
2017-02-14 16:20:07 +0100

    refactor(form-element): fully support all element types

--- src/component/form-element.js
+++ src/component/form-element.js
@@ -2,38 +2,67 @@ import {Configuration} from 'aurelia-config';
 import {inject, bindable, customElement, bindingMode, TemplatingEngine} from 'aurelia-framework';
 import {resolvedView} from 'aurelia-view-manager';
 import {logger} from '../aurelia-form';
+import {DOM} from 'aurelia-pal';
 
 @resolvedView('spoonx/form', 'form-element')
 @customElement('form-element')
-@inject(Configuration.of('aurelia-form'), Element, TemplatingEngine)
+@inject(Configuration.of('aurelia-form'), DOM.Element, TemplatingEngine)
 export class FormElement {
-  @bindable type = 'input';
-  @bindable model;
+  @bindable type = 'text';
+
   @bindable element;
+
+  @bindable DOMElement;
+
   @bindable({defaultBindingMode: bindingMode.twoWay}) value;
 
-  constructor(config, element, templatingEngine) {
+  @bindable name = '';
+
+  @bindable classes = '';
+
+  @bindable placeholder = '';
+
+  @bindable readonly = false;
+
+  @bindable disabled = false;
+
+  @bindable options = {};
+
+  proxyAttributes = [
+    'value',
+    'name',
+    'classes',
+    'placeholder',
+    'readonly',
+    'disabled',
+    'options',
+    'type'
+  ];
+
+  constructor(config, DOMElement, templatingEngine) {
     this.config           = config;
-    this.element          = element;
+    this.DOMElement       = DOMElement;
     this.templatingEngine = templatingEngine;
-    this.model            = this;
   }
 
   attached() {
-    let element = document.createElement(this.getElementName());
+    let element = DOM.createElement(this.getElementName());
 
-    element.setAttribute('value.bind', 'value');
-
-    this.element.appendChild(element);
+    this.setAttributes(element);
+    this.DOMElement.appendChild(element);
     this.templatingEngine.enhance({element, bindingContext: this});
   }
 
+  setAttributes(DOMElement) {
+    this.proxyAttributes.forEach(attribute => DOMElement.setAttribute(`${attribute}.bind`, attribute));
+  }
+
   getElementName() {
-    let elementName = this.config.elements[this.type];
+    let elementName = this.config.elements[this.element];
 
     if (!elementName) {
       logger.warn(
-        `Unable to find element for type '${this.type}'. Falling back to default '${this.config.defaultElement}'.`
+        `Unable to find element for element '${this.element}'. Falling back to default '${this.config.defaultElement}'.`
       );
 
       elementName = this.config.elements[this.config.defaultElement];
