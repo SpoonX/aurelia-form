a57d144ee253e6a92788c26ebf47b87c1eebf4f3
RWOverdijk <r.w.overdijk@gmail.com>
2017-02-14 16:35:42 +0100

    feat(element): add radio

---
+++ src/component/form-radio.js
@@ -0,0 +1,24 @@
+import {bindable, customElement, bindingMode} from 'aurelia-framework';
+import {resolvedView} from 'aurelia-view-manager';
+
+@resolvedView('spoonx/form', 'form-radio')
+@customElement('form-radio')
+export class FormRadio {
+  @bindable name;
+
+  @bindable({defaultBindingMode: bindingMode.twoWay}) value;
+
+  @bindable({defaultBindingMode: bindingMode.twoWay}) option;
+
+  @bindable type = 'text';
+
+  @bindable classes = '';
+
+  @bindable placeholder = '';
+
+  @bindable readonly = false;
+
+  @bindable disabled = false;
+
+  @bindable options = {};
+}
