8548567b1cc550919eeeb02a7c70ae7424be3819
jeremyvergnas <jeremy.vergnas@viacesi.fr>
2017-02-15 10:30:08 +0100

    doc(formCheckbox): add pointer for future improvement

--- src/component/form-checkbox.js
+++ src/component/form-checkbox.js
@@ -1,6 +1,12 @@
 import {bindable, customElement, bindingMode} from 'aurelia-framework';
 import {resolvedView} from 'aurelia-view-manager';
 
+/**
+ * FormCheckbox should inherit FormInput, so the bindables don't need to be copied.
+ *
+ * To achieve this, we will need this pull request to be merged:
+ * https://github.com/aurelia/templating/pull/507
+ */
 @resolvedView('spoonx/form', 'form-checkbox')
 @customElement('form-checkbox')
 export class FormCheckbox {
