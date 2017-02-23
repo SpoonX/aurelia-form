1735148c79f951d16e0ef5d63ceb64c1c08d130d
RWOverdijk <r.w.overdijk@gmail.com>
2017-02-23 16:28:10 +0100

    feat(component): add association component

---
+++ src/component/form-association.js
@@ -0,0 +1,14 @@
+import {bindable, customElement, bindingMode} from 'aurelia-framework';
+import {resolvedView} from 'aurelia-view-manager';
+
+@resolvedView('spoonx/form', 'form-association')
+@customElement('form-association')
+export class FormAssociation {
+  @bindable({defaultBindingMode: bindingMode.twoWay}) value;
+
+  @bindable options;
+
+  @bindable disabled;
+
+  @bindable placeholder;
+}
